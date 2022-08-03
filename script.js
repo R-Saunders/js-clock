2c325dd389275cf616eb5f1a9d30eea14bedf6e7
Crimson <richsaunders@protonmail.com>
2022-08-03 18:03:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
-
 function setDate() {
+  
+  const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+  
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
